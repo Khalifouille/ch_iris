daed4488f6a11890747801b466fea5aa37aded44
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-03 21:57:14 +0100

    c r

--- server.lua
+++ server.lua
@@ -1,6 +1,5 @@
 AddEventHandler('onResourceStart', function(resourceName)
     if GetCurrentResourceName() == resourceName then
-        print("Resource started: " .. resourceName)
         local pedModel = 'a_m_y_hipster_01'
         local x, y, z = 84.870331, -448.483521, 37.536255
         TriggerClientEvent('ch_iris:createPed', -1, pedModel, x, y, z)
